0523c4a7ee3810bdf19a5f0d9ff133dae9f76d73
kalbulka <kylebelka30@gmail.com>
2022-03-04 15:27:42 -0600

    Sect 2.3 added label

--- Reel yew ar not awsim/ViewController.swift
+++ Reel yew ar not awsim/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("👍 viewDidLoad has run!")
+        messageLabel.text = "Twilight"
     }
 
 
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        print("😎 The message button was pressed!")
+        messageLabel.text = "THE BAtman"
+    }
 }
 
